6f870f0e45134d19a9ec4c187217a605d4e946e0
Sebastian Beltran <beltran.sebastiang@gmail.com>
2024-05-29 23:27:00 -0500

    Added permision in android and ios

--- lib/main.dart
+++ lib/main.dart
@@ -1,66 +1,30 @@
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
-import 'dart:io';
+import 'package:flutter_gemini_ai/ui/home_screen.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: ImagePickerDemo(),
-    );
-  }
-}
-
-class ImagePickerDemo extends StatefulWidget {
-  @override
-  _ImagePickerDemoState createState() => _ImagePickerDemoState();
-}
-
-class _ImagePickerDemoState extends State<ImagePickerDemo> {
-  File? _image;
-
-  final ImagePicker _picker = ImagePicker();
-
-  Future<void> _pickImage(ImageSource source) async {
-    final pickedFile = await _picker.pickImage(source: source);
-
-    setState(() {
-      if (pickedFile != null) {
-        _image = File(pickedFile.path);
-      } else {
-        print('No image selected.');
-      }
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Image Picker Demo'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            _image == null ? Text('No image selected.') : Image.file(_image!),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () => _pickImage(ImageSource.camera),
-              child: Text('Take a Photo'),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () => _pickImage(ImageSource.gallery),
-              child: Text('Select from Gallery'),
-            ),
-          ],
+      title: 'Pasa palabra',
+      theme: ThemeData(
+        textTheme: const TextTheme(
+          bodyMedium: TextStyle(),
+        ).apply(
+          bodyColor: Colors.white,
         ),
+        appBarTheme: const AppBarTheme(
+          color: Colors.black,
+        ),
+        scaffoldBackgroundColor: Colors.black,
+        useMaterial3: true,
       ),
+      home: ImagePickerDemo(),
     );
   }
 }
--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -1,96 +1,59 @@
-// import 'dart:convert';
-// import 'dart:io';
-// import 'dart:ui' as imgui;
+import 'dart:io';
 
-// import 'package:flutter/material.dart';
-// import 'package:flutter_gemini_ai/env/env.dart';
-// import 'package:google_generative_ai/google_generative_ai.dart';
-// import 'package:image_picker/image_picker.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
-// class ImageAnalysisApp extends StatefulWidget {
-//   @override
-//   _ImageAnalysisAppState createState() => _ImageAnalysisAppState();
-// }
+class ImagePickerDemo extends StatefulWidget {
+  const ImagePickerDemo({super.key});
 
-// class _ImageAnalysisAppState extends State<ImageAnalysisApp> {
-//   late imgui.Image _selectedImage;
-//   String _imageDescription = '';
-//   final _imagePicker = ImagePicker();
+  @override
+  _ImagePickerDemoState createState() => _ImagePickerDemoState();
+}
 
-//   Future<void> _analyzeImage() async {
-//     if (_selectedImage == null) return;
+class _ImagePickerDemoState extends State<ImagePickerDemo> {
+  File? _image;
 
-//     // Analyze the image using Gemini API (code from previous response)
-//     final description = await analyzeImage(_selectedImage);
+  final ImagePicker _picker = ImagePicker();
 
-//     setState(() {
-//       _imageDescription = description;
-//     });
-//   }
+  Future<void> _pickImage(ImageSource source) async {
+    final pickedFile = await _picker.pickImage(source: source);
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('Image Analysis App'),
-//       ),
-//       body: Column(
-//         children: [
-//           // Image display area
-//           if (_selectedImage != null) Image.file(_selectedImage),
+    setState(() {
+      if (pickedFile != null) {
+        _image = File(pickedFile.path);
+      } else {
+        print('No image selected.');
+      }
+    });
+  }
 
-//           // Image selection button
-//           ElevatedButton(
-//             onPressed: () async {
-//               final image =
-//                   await _imagePicker.pickImage(source: ImageSource.gallery);
-//               if (image != null) {
-//                 setState(() {
-//                   _selectedImage = File('assets/estudio.avif');
-//                 });
-//               }
-//             },
-//             child: Text('Select Image'),
-//           ),
-
-//           // Analyze image button
-//           ElevatedButton(
-//             onPressed: _analyzeImage,
-//             child: Text('Analyze Image'),
-//           ),
-
-//           // Image description display
-//           Text(_imageDescription),
-//         ],
-//       ),
-//     );
-//   }
-
-//   Future<String> analyzeImage(imgui.Image image) async {
-//     // Initialize Gemini API
-//     final gemini = GenerativeModel(
-//       model: 'gemini-1.5-flash',
-//       apiKey: EnvPlatform.apiKey,
-//     );
-
-//     // Convert image to base64 encoded string
-//     final imageBytes =
-//         await image.toByteData(format: imgui.ImageByteFormat.png);
-//     final imageBase64 = imageBytes!.buffer.asUint8List();
-
-//     final prompt = TextPart("What's different between these pictures?");
-//     final imageParts = [
-//       DataPart('assets/estudio.avif', imageBase64),
-//     ];
-//     // Send image data to Gemini API
-//     final response = await gemini.generateContent([
-//       Content.multi([prompt, ...imageParts])
-//     ]);
-
-//     // Process the response and extract image description
-//     return response.text!;
-
-//     // Display the image description
-//     // Update UI to display the description
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Imagen',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            _image == null ? Text('No image selected.') : Image.file(_image!),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () => _pickImage(ImageSource.camera),
+              child: Text('Take a Photo'),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () => _pickImage(ImageSource.gallery),
+              child: Text('Select from Gallery'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
